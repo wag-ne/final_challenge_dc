2e9baa1f98f799f1781c01f2f99a91c61f430506
Wagner <wagdev@DESKTOP-5FKUCAS.localdomain>
2021-05-31 18:06:49 -0300

    test: make corrections on test and fix rubocop offenses

--- spec/services/token_service_spec.rb
+++ spec/services/token_service_spec.rb
@@ -4,7 +4,7 @@ describe TokenService do
   describe '#token' do
     before do
       URL = URI(ENV['TOKEN_API_URL'])
-      allow(Net::HTTP).to receive(:get).with(URL).and_return('{\"token\":\"MMF0YXRhKzIwMjEtMDUtMzFUMTM6NDE6NDcrMDA6MDA=\\n\"}')
+      allow(Net::HTTP).to receive(:get).with(URL).and_return("{\"token\":\"MMF0YXRhKzIwMjEtMDUtMzFUMTM6NDE6NDcrMDA6MDA=\\n\"}")
     end
 
     it 'return a valid token without cache' do
